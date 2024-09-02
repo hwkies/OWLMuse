942fbd8c26d93ac4eced50216280f02fc9ec1719
hwkies <harrykiesman@gmail.com>
2024-09-02 03:21:32 -0400

    navbar and sidebar

--- owl_model_app/src/app/app.component.ts
+++ owl_model_app/src/app/app.component.ts
@@ -2,14 +2,19 @@ import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { TuiButton, tuiButtonOptionsProvider } from '@taiga-ui/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {TuiButtonClose} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NavbarComponent, TuiRoot],
+  imports: [RouterOutlet, RouterModule, TuiRoot, MatToolbarModule, RouterModule, TuiButton, MatIconModule, MatSidenavModule, TuiButtonClose],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [tuiButtonOptionsProvider({size: 'm'})],
 })
 export class AppComponent {
   title = 'owl_model_app';
