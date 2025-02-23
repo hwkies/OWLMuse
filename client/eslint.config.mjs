3fb4b881aeb1976f22d77509ed438471f4ec26b7
hwkies <harrykiesman@gmail.com>
2025-02-22 20:27:03 -0500

    edited linting

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -7,10 +7,18 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    extends: [],
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
+  }),
 ];
 
 export default eslintConfig;
--- client/src/components/main/navbar/index.tsx
+++ client/src/components/main/navbar/index.tsx
@@ -1,21 +1,22 @@
 import * as React from "react"
 import { Input } from "@chakra-ui/react"
+import Link from "next/link"
 
 const NavBar = () => {
   return (
     <nav className="bg-cyan-500 grid grid-cols-3 items-center px-4 py-2">
       <div className="justify-self-start">
-        <a href="/" className="text-white font-bold text-2xl">
+        <Link href="/" className="text-white font-bold text-2xl">
           OWLMuse
-        </a>
+        </Link>
       </div>
       <div className="justify-self-center min-w-full">
         <Input placeholder="Search for overwatch statistics" className="bg-white px-3 rounded-2xl border-4" />
       </div>
       <div className="justify-self-end">
-        <a href="/login" className="text-white font-semibold text-2xl">Log In</a>
+        <Link href="/login" className="text-white font-semibold text-2xl">Log In</Link>
         <span className="text-white font-semibold text-3xl"> | </span>
-        <a href="/signup" className="text-white font-semibold text-2xl">Sign Up</a>
+        <Link href="/signup" className="text-white font-semibold text-2xl">Sign Up</Link>
       </div>
     </nav>
   );
